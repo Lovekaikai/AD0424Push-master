5859ee9bc1236a3aac352c855b607e8b1c0eed7e
kai <1145275516@qq.com>
2017-06-15 09:54:10 +0800

    kai

---
+++ app/src/test/java/ad0424/yls/example/com/pushdemo/ExampleUnitTest.java
@@ -0,0 +1,12 @@
+package ad0424.yls.example.com.pushdemo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
